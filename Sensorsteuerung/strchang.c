e07744be39f59210edab605d65fe3946dfdc9d16
TheWalkingGiant <Severin@The-Walking-Giant.local>
2015-05-19 18:22:27 +0200

    Initialisieren von strchang

--- Sensorsteuerung/strchang.c
+++ Sensorsteuerung/strchang.c
@@ -1,4 +1,27 @@
 #include "strchang.h";
 #include <string.h>;
 #include <Windows.h>;
+#include <cstring.h>;
 
+int stringverarbeitung(char[] messung, int gerät) {
+	char[] messkopie();
+	strcpy_s(messkopie, sizeof(messung), messung);
+
+	if (gerät == 1)
+	{
+		for (int i = 0; i <=2 ; ++i)
+		{
+			
+		}
+	}
+
+	if (gerät == 2)
+	{
+		/* code */
+	}
+
+	else {
+		printf("Gerät wurde nicht erkannt und String nicht bearbeitet\n");
+		return 0;
+	}
+}
