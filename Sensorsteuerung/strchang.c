dc01d05972a4d38a4ee10e09f1511c2cd19a5f4f
Andifan <andi_9407@gmx.de>
2015-05-19 23:06:56 +0200

    Bugs behoben Teilstring - Funktion. Nun voll Funktional

--- Sensorsteuerung/strchang.c
+++ Sensorsteuerung/strchang.c
@@ -1,11 +1,9 @@
-#include "strchang.h";
-#include <string.h>;
-#include <Windows.h>;
-#include <cstring.h>;
 
 int stringverarbeitung(char[] messung, int gerät) {
-	char messkopie[];
-	strcpy_s(messkopie, sizeof(messung), messung);
+
+	if (strcmp(teilstring(0, 2, &messung[0]), "RES") == 0){
+		// hier Quellcode einfügen
+	}
 
 	if (gerät == 1)
 	{
@@ -26,13 +24,20 @@ int stringverarbeitung(char[] messung, int gerät) {
 	}
 }
 
-void teilstring(int anfang, int ende, char messung[], char *ps) {
-	char teil[ende - anfang];
-	for (int i = anfang; i <= ende; ++i)
-	{
-		teil[i - anfang] = messung[i];
+char *teilstring(int anfang, int ende, char *ps1){
+	char *ps = malloc(ende - anfang); // free muss noch hinzugefüght werden
+	printf("%i \n", sizeof(ps1));
+	char teil[sizeof(ps1)]; // es muss immer ein zeichen mehr platz definiert werden um platz für die \0 zu haben: 3 ausgelesene Elemente brauchen für 4 Platz
+	ps1 = ps1 + anfang;
+	int i;
+	for (i = anfang; i <= ende; i++) {
+		teil[i - anfang] = *ps1;
+		ps1++;
 	}
-
+	teil[i - anfang] = '\0'; // schließt String ab
+	printf("%s \n", teil);
 	strcpy(ps, teil);
-	
+	return ps;
+
+
 }
